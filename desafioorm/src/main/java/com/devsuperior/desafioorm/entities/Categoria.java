269e58ef59a84d5b57279f95372e70b6f25006a7
carlosdiego369 <carlosdiegofs@live.com>
2023-07-27 09:26:23 -0300

    Projeto desafio ORM + Banco H2 configurado

---
+++ desafioorm/src/main/java/com/devsuperior/desafioorm/entities/Categoria.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioorm.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+}
